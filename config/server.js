71be823d7e92e50cec0a4e418e7cb6f2f8409b0f
Güngör Öztürk <55546472+gngrozturk@users.noreply.github.com>
2020-10-20 19:54:38 +0300

    Heroku

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'df07e13546062b6cd9c40e610153f1cb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c8bdff47-9c79-440d-a86e-62d4581e514e'
+};
